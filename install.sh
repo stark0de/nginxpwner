cbc089832b7d3c6e0238222315fc7e88c7dbcf2d
stark0de <53110298+stark0de@users.noreply.github.com>
2021-04-06 18:48:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/shibli2700/Kyubi.git
-cd Kyubi && sudo python3 setup.py install && pip3 install -r requirements.txt
+cd Kyubi && sudo python3 setup.py install && pip3 install -r /opt/nginxpwner/requirements.txt
